0bd8cdc7dd0503c6c077b525617cd3f48f64524b
scottorr-4 <orrs@oregonstate.edu>
2025-02-22 18:14:51 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,25 @@
 // Map 2
 
-var map2, geojson2;
+var map, geojson;
 
-// Define the info control for map2
-var info2 = L.control();
+// Define the info control for map
+var info = L.control();
 
-info2.onAdd = function (map2) {
+info.onAdd = function (map) {
     this._div = L.DomUtil.create('div', 'info'); // Create a div with a class "info"
     this.update();
     return this._div;
 };
 
 // Method to update the control based on feature properties passed
-info2.update = function (props) {
+info.update = function (props) {
     this._div.innerHTML = '<h4>U.S. Annual Emissions</h4>' + (props ?
         '<b>' + props.name + '</b><br />' + props['Variable observation value'] + ' metric tons of CO<sub>2</sub>' :
         'Hover over a state');
 };
 
 // Function to highlight features on mouseover
-function highlightFeature2(e) {
+function highlightFeature(e) {
     var layer = e.target;
 
     layer.setStyle({
@@ -31,33 +31,33 @@ function highlightFeature2(e) {
 
     layer.bringToFront();
 
-    // Update the info control for map2
-    info2.update(layer.feature.properties);
+    // Update the info control for map
+    info.update(layer.feature.properties);
 }
 
 // Function to reset the highlight on mouseout
-function resetHighlight2(e) {
-    geojson2.resetStyle(e.target);
-    info2.update(); // Reset the info control for map2
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+    info.update(); // Reset the info control for map
 }
 
 // Function to zoom to a feature on click
-function zoomToFeature2(e) {
-    map2.fitBounds(e.target.getBounds());
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
 }
 
 // Function to handle each feature
-function onEachFeature2(feature, layer) {
+function onEachFeature(feature, layer) {
     layer.on({
-        mouseover: highlightFeature2,
-        mouseout: resetHighlight2,
-        click: zoomToFeature2
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
     });
 }
 
-// Function to create map2
-function createMap2() {
-    map2 = L.map('map2', {
+// Function to create map
+function createMap() {
+    map = L.map('map', {
         center: [39.83, -98.58],
         zoom: 4
     });
@@ -75,9 +75,9 @@ function createMap2() {
     // Add OSM base tile layer
     L.tileLayer('http://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-    }).addTo(map2);
+    }).addTo(map);
 
-    // Add GeoJSON layer to map2 with styling and event handlers
+    // Add GeoJSON layer to map with styling and event handlers
     fetch("data/state_emissions.json")
         .then(function (response) {
             return response.json();
@@ -86,11 +86,11 @@ function createMap2() {
             // Use the JSON data to create the geojson2 layer
             geojson2 = L.geoJson(json, { 
                 style: style,
-                onEachFeature: onEachFeature2
-            }).addTo(map2);
+                onEachFeature: onEachFeature
+            }).addTo(map);
 
-            // Add info control to map2
-            info2.addTo(map2);
+            // Add info control to map
+            info2.addTo(map);
         })
         .catch(function (error) {
             console.error("Error loading the data: ", error);
@@ -99,5 +99,5 @@ function createMap2() {
 
 // Initialize the map when the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', function() {
-    createMap2();
+    createMap();
 });
